c1ac6dbec712849c36f1e333ed4c9c3473606038
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-07-08 20:34:37 -0400

    Put the inquirer inside the init function; changed the way of how to create direction of where file should be created

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-//const {renderLicenseLink, renderLicenseBadge, renderLicenseSection } = require('./utils/generateMarkdown.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
-//const { generateMarkdown, renderLicenseLink, renderLicenseBadge, renderLicenseSection } = require('./utils/generateMarkdown.js');
-//const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // TODO: Create a function to write README file
@@ -30,25 +27,20 @@ ${answers.test}
 LinkedIn: ${answers.linkedIn}
 GitHub: ${answers.gitHub}
 Email: ${answers.email}
-
-## License
-    [License Link](${renderLicenseLink(answers.license)})
+${generateMarkdown.renderLicenseLink(answers.license)}
 `;
- // Write the content to a README file
-  fs.writeFile('README.md', content, (err) => {
-      if (err) {
-          console.error(err);
-          return;
-      }
-      console.log('README.md file created successfully!');
-  });
-}
-// Function to save the generated README file
-const saveREADME = (md) => {
-  //console.log(md);
-  fs.writeFileSync("newREADME.md", md);
-  console.log("Successfully written!");
 
+  if (!fs.existsSync('./output')) {
+    fs.mkdirSync('./output');
+  }
+  // Write the content to a README file
+  fs.writeFile('output/README.md', content, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('README.md file created successfully!');
+  });
 }
 
 
@@ -56,100 +48,89 @@ const saveREADME = (md) => {
 //const userResponses = [];
 // Array of questions for user input
 const questions = [
-    {
-        type:"input",
-        name:"name",
-        message:"Enter your projects title", 
-        default: "New Application",       
-    },
-    {
-        type:"input",
-        name:"location",
-        message:"Where your application was created",   
-        default: "Earth Planet"     
-    },
-    {
-        type:"input",
-        name:"description",
-        message:"Describe your project breafly",
-        default: "To make our life better",
-    },
-    {
-        type:"input",
-        name:"installation",
-        message:"How to install your application",
-        default: "Download from some site",
-    },
-    {
-        type:"input",
-        name:"usage",
-        message:"What is the usage of your program",
-        default: "On your on choise",
-    },
-    {
-        type:"input",
-        name:"contributing",
-        message:"How to contribute to your project",
-        default: "Find us on gitHub",
-    },
-    {
-        type:"input",
-        name:"test",
-        message:"How to test your app",
-        default: "Free",
-    },
-    {
-        type:"input",
-        name:"linkedIn",
-        message:"Enter LinkedIn handle",    
-        default: "nariman-suleimenov-61ba422b8/",    
-    },
-    {
-        type:"input",
-        name:"gitHub",
-        message:"Enter GitHub username",   
-        default: "NarSulEsz",     
-    },
-    {
-        type:"input",
-        name:"email",
-        message:"Write your email",   
-        default: "noadress@gmail.com",     
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Choose a license for your project",
-      choices: ["MIT", "Apache", "GPL"],
-      default: "MIT"
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your projects title",
+    default: "New Application",
+  },
+  {
+    type: "input",
+    name: "location",
+    message: "Where your application was created",
+    default: "Earth Planet"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project breafly",
+    default: "To make our life better",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install your application",
+    default: "Download from some site",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage of your program",
+    default: "On your on choise",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How to contribute to your project",
+    default: "Find us on gitHub",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How to test your app",
+    default: "Free",
+  },
+  {
+    type: "input",
+    name: "linkedIn",
+    message: "Enter LinkedIn handle",
+    default: "nariman-suleimenov-61ba422b8/",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "Enter GitHub username",
+    default: "NarSulEsz",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Write your email",
+    default: "noadress@gmail.com",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project",
+    choices: ["MIT", "Apache", "GPL"],
+    default: "MIT"
   },
 ];
-// Prompt the user for input based on the questions array
-inquirer.prompt(questions)
-  .then(answers => {
-    console.log('User Responses:');
-    console.log(answers);
-    // You can further process the user responses here
-  generateREADME(answers);
-    })
-   .catch(error => {
-    console.error('Error occurred:', error);
-  })
-
-  
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  // Prompt the user for input based on the questions array
+  inquirer.prompt(questions)
+    .then(answers => {
+      console.log('User Responses:');
+      console.log(answers);
+      // You can further process the user responses here
+      generateREADME(answers);
+    })
+    .catch(error => {
+      console.error('Error occurred:', error);
+    })
+}
 
 // Function call to initialize app
 init();
-
-
-const content = `# ${answers.name}
-${renderLicenseBadge(answers.license)}
-${renderLicenseLink(answers.license)}
-${renderLicenseSection(answers.license)}
-
-## Description
-${answers.description}
-`
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,41 +2,42 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
   } else if (license === 'Apache') {
-      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
   } else if (license === 'GPL') {
-      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
   } else {
-      return '';
+    return '';
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { if (license === 'MIT') {
-  return '[MIT License](https://opensource.org/licenses/MIT)';
-} else if (license === 'Apache') {
-  return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
-} else if (license === 'GPL') {
-  return '[GNU GPL v3.0 License](https://www.gnu.org/licenses/gpl-3.0)';
-} else {
-  return '';
-}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT License](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache') {
+    return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPL') {
+    return '[GNU GPL v3.0 License](https://www.gnu.org/licenses/gpl-3.0)';
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'MIT') {
-  return 'This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.';
-} else if (license === 'Apache') {
-  return 'This project is licensed under the Apache License 2.0. See the [LICENSE](LICENSE) file for details.';
-} else if (license === 'GPL') {
-  return 'This project is licensed under the GNU General Public License v3.0. See the [LICENSE](LICENSE) file for details.';
-} else {
-  return '';
-}
+    return 'This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.';
+  } else if (license === 'Apache') {
+    return 'This project is licensed under the Apache License 2.0. See the [LICENSE](LICENSE) file for details.';
+  } else if (license === 'GPL') {
+    return 'This project is licensed under the GNU General Public License v3.0. See the [LICENSE](LICENSE) file for details.';
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function to generate markdown for README
